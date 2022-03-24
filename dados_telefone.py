886adc3d3f2058aadca18d583e5775b54c5e97c5
Allan <aln.porfirio@gmail.com>
2022-03-24 11:26:29 -0300

    initial commit

--- dados_telefone.py
+++ dados_telefone.py
@@ -3,7 +3,7 @@
 import phonenumbers
 from phonenumbers import geocoder, carrier  # descobrir a localização estadual
 
-telefone = input(" Digite o numero do telefone fixo ou celular: ")# receber o número do telefone
+telefone = input(" Digite o numero do telefone fixo ou celular: ") # receber o número do telefone
 num_tel = phonenumbers.parse(telefone, 'BR') #aqui colocando BR dispensamos a necessidade do +55
 num_valido = phonenumbers.is_valid_number(num_tel) #validando a existencia do numero.
 operadora = carrier.name_for_number(num_tel,'pt-br')
