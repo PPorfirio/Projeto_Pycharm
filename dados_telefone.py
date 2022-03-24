3a158dc449264bdeb10832aee9451c7d8af35025
Allan <aln.porfirio@gmail.com>
2022-03-24 10:38:41 -0300

    initial commit

---
+++ dados_telefone.py
@@ -0,0 +1,17 @@
+# código para descobrir alguns dados de telefone usando a biblioteca phonenumbers, antes instale o pacote pip install phonenumbers
+
+import phonenumbers
+from phonenumbers import geocoder, carrier  # descobrir a localização estadual
+
+telefone = input(" Digite o numero do telefone fixo ou celular: ")# receber o número do telefone
+num_tel = phonenumbers.parse(telefone, 'BR') #aqui colocando BR dispensamos a necessidade do +55
+num_valido = phonenumbers.is_valid_number(num_tel) #validando a existencia do numero.
+operadora = carrier.name_for_number(num_tel,'pt-br')
+if num_valido == True:
+    print(telefone)
+    print(geocoder.description_for_number(num_tel,'pt'))
+    print(operadora)
+    print(num_valido)
+else:
+    print('/_\ Você digitou um numero que não existe, tente novamente')
+
